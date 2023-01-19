f635775e96094113878b53807ef08861329be7e9
Walid@DESKTOP-K7L8K6O <Walid@DESKTOP-K7L8K6O>
2023-01-19 23:09:24 +0100

    fin tp2

--- chart/public/scripts/myChart.js
+++ chart/public/scripts/myChart.js
@@ -3,22 +3,17 @@ const nbValues = 12;
 const defaultValue = 1;
 const MIN_VALUE = 0;
 const MAX_VALUE = 10;
-let value = 0;
 
-
-const allLabels = new Array(nbValues).fill(defaultValue).map( (_,i) => String.fromCharCode('A'.charCodeAt(0)+i));
-//const allLabels = ['J','F','M','A','M','J','J','A','S','O','N','D'];
-
-// const displayMessage = msg => document.getElementById('messageZone').appendChild(buildMessage(msg));
 const socket = io();
 socket.on('ping', () => console.log("ping")); //send ping from server
 socket.emit("pong"); //send pong to server
-socket.on("val", (arg) => {console.log(arg);});
+
+const allLabels = new Array(nbValues).fill(defaultValue).map( (_,i) => String.fromCharCode('A'.charCodeAt(0)+i));
+
 
 const ctxt = document.getElementById('myChart').getContext('2d');
 
-// l'objet Chart
-const myChart = new Chart(ctxt, {
+let myChart = new Chart(ctxt, {
     type: 'bar',
     data: {
         labels: allLabels,
@@ -41,20 +36,13 @@ const myChart = new Chart(ctxt, {
     }
   });
 
-  function addData(chart, label, data) {
-    chart.data.labels.unshift(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.unshift(data);
-    });
-    removeData(chart);
-    chart.update();
-}
 
-function removeData(chart) {
-  chart.data.labels.pop();
-  chart.data.datasets.forEach((dataset) => {
-      dataset.data.pop();
-  });
-  chart.update();
+  function addData(res) {
+    myChart.data.labels.unshift(myChart.data.labels.pop());
+    myChart.data.datasets.forEach((dataset) => {
+        dataset.data.unshift(dataset.data.pop());
+        dataset.data[0] = res;
+    });
+      myChart.update();
 }
-addData(myChart, '1', myChart.data.datasets.data);
+socket.on('val', (arg) => {console.log(arg); addData(arg);});
