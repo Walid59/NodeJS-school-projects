5616959dcd1e452eb391ff64bb63c095c7978f36
walid.rouabah.etu <walid.rouabah.etu@univ-lille.fr>
2023-01-20 11:27:49 +0100

    mise a jour README et réalisation tp2

--- chart/controllers/ioController.js
+++ chart/controllers/ioController.js
@@ -1,12 +1,13 @@
 // fichier ./controllers/ioController.js (serveur)
 export default class IOController {
     #io;
-    #clients;
     #intervalID;
+    static #clients = new Map();
+    //static #intervalID = setInterval(IOController.sendRandomVal.bind(IOController), 2000);
+
 
     constructor(io) {
         this.#io = io;
-        this.#clients = new Map();
     }
 
     registerSocket(socket) {
@@ -23,19 +24,29 @@ export default class IOController {
 
     leave(socket) {
         console.log(`disconnection from ${socket.id}`);
-        clearInterval(this.#intervalID);
+        clearInterval(IOController.#clients.delete(socket));
     }
 
+/*      static sendRandomVal(){
+        const randomVal = IOController.RandomIntInclusive(2,8);
+        IOController.#clients.forEach((values,keys)=>{
+            keys.emit("val",randomVal);
+          })
+    } */
+
     sendRandomVal(socket){
-        const randomVal = this.RandomIntInclusive(2,8);
+        const randomVal = IOController.RandomIntInclusive(2,8);
         socket.emit("val",randomVal);
     }
 
+
     setupInterval(socket){
         this.#intervalID = setInterval(this.sendRandomVal.bind(this), 2000, socket);
-    }
+        //IOController.#clients.set(socket , IOController.#intervalID);
+        IOController.#clients.set(socket , this.#intervalID);
+    } 
 
-    RandomIntInclusive(min, max) {
+    static RandomIntInclusive(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min +1)) + min;
